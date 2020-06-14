2e6ea86aacc7e173a1f0e81b3228d0328b4fb417
Thanh Nguyen <tienthanh8490@gmail.com>
2020-06-14 20:37:07 +0200

    copy files or dir

---
+++ copy-file-or-dir.js
@@ -0,0 +1,18 @@
+/* global ipfs, all */
+
+// https://proto.school/#/mutable-file-system/09
+
+const run = async (files) => {
+  await Promise.all(files.map(f => ipfs.files.write('/' + f.name, f, { create: true })))
+  await ipfs.files.mkdir('/some/stuff', { parents: true })
+  const rootDirectoryContents = await all(ipfs.files.ls('/'))
+  const filepathsToMove = rootDirectoryContents.filter(file => file.type === 0).map(file => '/' + file.name)
+  await ipfs.files.mv(filepathsToMove, '/some/stuff')
+
+  await ipfs.files.cp('/ipfs/QmWCscor6qWPdx53zEQmZvQvuWQYxx1ARRCXwYVE4s9wzJ', '/some/stuff/success.txt')
+
+  let someStuffDirectoryContents = await all(ipfs.files.ls('/some/stuff'))
+  return someStuffDirectoryContents
+}
+
+return run
